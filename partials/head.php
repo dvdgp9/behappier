7c50f40d05a95320efc6686b80bd5c43d4b00343
David Gutiérrez <dvdgp93@gmail.com>
2025-08-19 00:09:08 +0200

    Ajustes estilo PWA 8

--- partials/head.php
+++ partials/head.php
@@ -17,7 +17,17 @@ declare(strict_types=1);
   <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="assets/styles.css">
 </head>
-<body>
+<?php
+  // Add body class based on current page
+  $bodyClass = '';
+  if (isset($_SERVER['SCRIPT_NAME'])) {
+    $page = basename($_SERVER['SCRIPT_NAME'], '.php');
+    if ($page === 'home') {
+      $bodyClass = 'home-page';
+    }
+  }
+?>
+<body<?= $bodyClass ? ' class="' . $bodyClass . '"' : '' ?>>
 <header class="site-header">
   <div class="container header-inner">
     <div class="brandline">
